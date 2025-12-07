3da692f0a17e088ee905c8336eca3c088058cb9d
Alex Marques <dianacastromm@gmail.com>
2025-12-07 13:54:41 -0300

    nome do usuario na tela

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -22,7 +22,7 @@ var db_usuarios = {};
 var usuarioCorrente = {};
 
 // Inicializa a aplicação de Login
-function initLoginApp () {
+function initLoginApp() {
     let pagina = window.location.pathname;
     if (pagina != LOGIN_URL) {
         // CONFIGURA A URLS DE RETORNO COMO A PÁGINA ATUAL
@@ -32,7 +32,7 @@ function initLoginApp () {
         // INICIALIZA USUARIOCORRENTE A PARTIR DE DADOS NO LOCAL STORAGE, CASO EXISTA
         usuarioCorrenteJSON = sessionStorage.getItem('usuarioCorrente');
         if (usuarioCorrenteJSON) {
-            usuarioCorrente = JSON.parse (usuarioCorrenteJSON);
+            usuarioCorrente = JSON.parse(usuarioCorrenteJSON);
         } else {
             window.location.href = LOGIN_URL;
         }
@@ -43,7 +43,7 @@ function initLoginApp () {
         // VERIFICA SE A URL DE RETORNO ESTÁ DEFINIDA NO SESSION STORAGE, CASO CONTRARIO USA A PÁGINA INICIAL
         let returnURL = sessionStorage.getItem('returnURL');
         RETURN_URL = returnURL || RETURN_URL
-        
+
         // INICIALIZA BANCO DE DADOS DE USUÁRIOS
         carregarUsuarios(() => {
             console.log('Usuários carregados...');
@@ -54,19 +54,19 @@ function initLoginApp () {
 
 function carregarUsuarios(callback) {
     fetch(API_URL)
-    .then(response => response.json())
-    .then(data => {
-        db_usuarios = data;
-        callback ()
-    })
-    .catch(error => {
-        console.error('Erro ao ler usuários via API JSONServer:', error);
-        displayMessage("Erro ao ler usuários");
-    });
+        .then(response => response.json())
+        .then(data => {
+            db_usuarios = data;
+            callback()
+        })
+        .catch(error => {
+            console.error('Erro ao ler usuários via API JSONServer:', error);
+            displayMessage("Erro ao ler usuários");
+        });
 }
 
 // Verifica se o login do usuário está ok e, se positivo, direciona para a página inicial
-function loginUser (login, senha) {
+function loginUser(login, senha) {
 
     // Verifica todos os itens do banco de dados de usuarios 
     // para localizar o usuário informado no formulario de login
@@ -81,7 +81,7 @@ function loginUser (login, senha) {
             usuarioCorrente.nome = usuario.nome;
 
             // Salva os dados do usuário corrente no Session Storage, mas antes converte para string
-            sessionStorage.setItem ('usuarioCorrente', JSON.stringify (usuarioCorrente));
+            sessionStorage.setItem('usuarioCorrente', JSON.stringify(usuarioCorrente));
 
             // Retorna true para usuário encontrado
             return true;
@@ -93,12 +93,12 @@ function loginUser (login, senha) {
 }
 
 // Apaga os dados do usuário corrente no sessionStorage
-function logoutUser () {
-    sessionStorage.removeItem ('usuarioCorrente');
+function logoutUser() {
+    sessionStorage.removeItem('usuarioCorrente');
     window.location = LOGIN_URL;
 }
 
-function addUser (nome, login, senha, email) {
+function addUser(nome, login, senha, email) {
 
     // Cria um objeto de usuario para o novo usuario 
     let usuario = { "login": login, "senha": senha, "nome": nome, "email": email };
@@ -114,7 +114,7 @@ function addUser (nome, login, senha, email) {
         .then(response => response.json())
         .then(data => {
             // Adiciona o novo usuário na variável db_usuarios em memória
-            db_usuarios.push (usuario);
+            db_usuarios.push(usuario);
             displayMessage("Usuário inserido com sucesso");
         })
         .catch(error => {
@@ -123,19 +123,20 @@ function addUser (nome, login, senha, email) {
         });
 }
 
-function showUserInfo (element) {
+function showUserInfo(element) {
     var elemUser = document.getElementById(element);
     if (elemUser) {
-            // Exibe apenas o ícone de logout, sem texto de usuário
-            elemUser.innerHTML = `
+        // Exibe apenas o ícone de logout, sem texto de usuário
+        elemUser.innerHTML = `
+                        ${usuarioCorrente.nome}
                         <a onclick="logoutUser()" style="cursor: pointer;" aria-label="Sair"> 
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                             <path d="M16 12.771h-3.091c-.542 0-.82-.188-1.055-.513l-1.244-1.674-2.029 2.199 1.008 1.562c.347.548.373.922.373 1.42v4.235h-1.962v-3.981c-.016-1.1-1.695-2.143-2.313-1.253l-1.176 1.659c-.261.372-.706.498-1.139.498h-3.372v-1.906l2.532-.001c.397 0 .741-.14.928-.586l1.126-2.75c.196-.41.46-.782.782-1.102l2.625-2.6-.741-.647c-.223-.195-.521-.277-.812-.227l-2.181.381-.342-1.599 2.992-.571c.561-.107 1.042.075 1.461.462l2.882 2.66c.456.414.924 1.136 1.654 2.215.135.199.323.477.766.477h2.328v1.642zm-2.982-5.042c1.02-.195 1.688-1.182 1.493-2.201-.172-.901-.96-1.528-1.845-1.528-1.186 0-2.07 1.078-1.85 2.234.196 1.021 1.181 1.69 2.202 1.495zm4.982-5.729v15l6 5v-20h-6z"/>
                         </svg>
-                    </a>`;
+                        </a>`;
     }
 }
 
 
 // Inicializa as estruturas utilizadas pelo LoginApp
-initLoginApp ();
+initLoginApp();
